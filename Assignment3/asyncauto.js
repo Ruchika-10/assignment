6e532f0cbf9dd7fc033665715f6f1f38f521c37c
Ruchika <42299989+Ruchika-10@users.noreply.github.com>
2019-10-01 11:05:52 +0530

    Create asyncauto.js

---
+++ Assignment3/asyncauto.js
@@ -0,0 +1,20 @@
+var async=require('async');
+a:function(callback){
+console.log('using a');
+callback(null,'a');
+}
+b:function(callback){
+console.log('using b');
+callback(null,'b');
+}
+c:['a','b',function(callback,results){
+console.log('a'+'b');
+callback(null,'c');
+}],
+},
+function(err,results)
+{
+console.log('err=',err);
+console.log('results=',results);
+}
+};
